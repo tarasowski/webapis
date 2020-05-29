f436c356b3c7420f3113b4bb15aca1477a7c30a0
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2020-05-29 10:17:58 +0200

    Update full-dom.js

--- full-dom.js
+++ full-dom.js
@@ -2,6 +2,15 @@
 // What is DOM
 // **********************
 
+// The Document Object Model (DOM) connects web pages to scripts or programming languages by representing the structure 
+// of a document—such as the HTML representing a web page—in memory. Usually that means JavaScript, although modeling HTML, 
+// SVG, or XML documents as objects is not part of the core JavaScript language, as such.
+
+// The DOM represents a document with a logical tree. Each branch of the tree ends in a node, and each node contains objects. 
+// DOM methods allow programmatic access to the tree. With them you can change the document's structure, style, or content.
+// Nodes can also have event handlers attached to them. Once an event is triggered, the event handlers get executed.
+// Source: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+
 // DOM is an interfrace that allows a programming languge to manipulate content, structure and style of a website
 // JavaScript is the client-side scripting language that connects to the DOM in an internet browser
 // A website consist of the **HTML document**:
